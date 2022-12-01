15f37ce7a4f4fd81677c1856f610f5a9d2dedaa9
59013-26-03 <59013-26-03@exchange.ad.afpanet>
2022-12-01 15:42:32 +0100

    exo 1 td3

--- td3/Exercice1.java
+++ td3/Exercice1.java
@@ -4,8 +4,17 @@ public class Exercice1 {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		
+		int somme = 0;
+		
 		System.out.println("Saisir la taille du tableau : ");
 		int [] tab = new int[Integer.parseInt(sc.nextLine())];	
 		
+		for(int i = 0; i < tab.length; i++) {
+			System.out.print("Saisir l'element " + i + " : ");
+			tab[i] = Integer.parseInt(sc.nextLine());
+			somme += tab[i];	
+		}
+		
+		System.out.print(somme);
 	}
 }
