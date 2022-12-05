67f277c31c6c1dd41be767e96d26dbc3fad3a9d9
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-12-05 21:34:11 +0100

    ex13 td3, refactoring

--- td3/Exercice13.java
+++ td3/Exercice13.java
@@ -4,28 +4,14 @@ public class Exercice13 {
 	public static void main(String args[]){
 		
 		Scanner sc = new Scanner(System.in);
-		int i;
-		int nbre;
-		int tmp;
+		System.out.print("Saisir la taille du tableau : ");
+		int tailleTableau = Integer.parseInt(sc.nextLine());
 		
-		int[] tab = new int[5];
-			
-		for(i=0;i<tab.length;i++){
-			System.out.println("Saisir des nombres : ");
-			nbre = Integer.parseInt(sc.nextLine());
-			tab[i] = nbre;
-		}
-
-		for(i=0;i<tab.length-1;i++){
-			if(tab[i] > tab[i+1]){
-				tmp = tab[i+1];
-				tab[i+1] = tab[i];
-				tab[i] = tmp;
-			}
-		}
+		int[] tab = TableauUtils.generationAleatoire(tailleTableau);
+		TableauUtils.afficher(tab);
 		
-		for(i=0;i<tab.length;i++){
-			System.out.print(" " + tab[i] + " ");
-		}
+		TableauUtils.trier(tab, true);
+		
+		TableauUtils.afficher(tab);		
 	}
 }
